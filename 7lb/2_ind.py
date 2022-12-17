75744a6d73a0b264fa9911adedafbed28155fc58
ProkDmit <dmidtriypdv@gmail.com>
2022-12-17 16:20:19 +0300

    Отчет

--- 7lb/2_ind.py
+++ 7lb/2_ind.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python3
 #-*- coding: utf-8 -*-
 if __name__=='__main__':
-    A=list(map(int, input("Введите целые элементы\n").split()))
-    p, s, k1, k2=1, 0, 0, 0
+    A=list(map(int, input("Введите оценки по алгебре\n").split()))
+    G=list(map(int, input("Введите оценки по геометрии\n").split()))
+    F=list(map(int, input("Введите оценки по физике\n").split()))
+    a=g=f=0
     for i in range(len(A)):
-        if i%2==0:
-            p*=A[i]
-        if A[i]==0:
-            if k1==0:
-                k1=i
-            k2=i
-    print("Произведение элементов списка с четными номерами:", p)
-    for i in range(len(A)):
-        if A[i]>k1 and A[i]<k2:
-            s+=A[i]
-    print("Сумма элементов между первым и последним 0 элементом:", s)
-    so=sorted(list(filter(lambda x: x>=0, A)))+sorted(list(filter(lambda x: x<0, A)))
-    print(so)
+        if A[i]>G[i] and A[i]>F[i]:
+            a+=1
+        if G[i]>A[i] and G[i]>F[i]:
+            g+=1
+        if F[i]>A[i] and F[i]>G[i]:
+            f+=1
+    print("Кол-во оценок по алгебре лучше:", a)
+    print("Кол-во оценок по геометрии лучше:", g)
+    print("Кол-во оценок по физике лучше:", f)
+    if a>g and a>f:
+        print("Лучшие оценки по алгебре")
+    if g>a and g>f:
+        print("Лучшие оценки по геометри")
+    if f>a and f>g:
+        print("Лучшие оценки по физике")
