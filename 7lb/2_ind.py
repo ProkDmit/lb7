8ee971dda2c641cd398de1c1bcd55c7fc9d20b7e
ProkDmit <dmidtriypdv@gmail.com>
2022-12-17 15:24:22 +0300

    Отчет

--- 7lb/2_ind.py
+++ 7lb/2_ind.py
@@ -1,25 +1,19 @@
 #!/usr/bin/env python3
 #-*- coding: utf-8 -*-
 if __name__=='__main__':
-    A=list(map(int, input("Введите элементы:\n").split()))
-    s1, s2, k1, k2=0, 0, 0, 0
+    A=list(map(int, input("Введите целые элементы\n").split()))
+    p, s, k1, k2=1, 0, 0, 0
     for i in range(len(A)):
-        if i%2!=0:
-            s1+=A[i]
-        if A[i]<0:
+        if i%2==0:
+            p*=A[i]
+        if A[i]==0:
             if k1==0:
                 k1=i
             k2=i
-    print("Сумма элементом с нечетными номерами:", s1)
+    print("Произведение элементов списка с четными номерами:", p)
     for i in range(len(A)):
-        if i>k1 and i<k2:
-            s2+=A[i]
-    print("Сумма элементом между 1 и последним отрицательными элементами", s2)
-    c=0
-    for i in range(len(A)):
-        if A[i]>1:
-            del A[i]
-            c+=1
-    for i in range(c):
-        A.append(0)
-    print(A)
+        if A[i]>k1 and A[i]<k2:
+            s+=A[i]
+    print("Сумма элементов между первым и последним 0 элементом:", s)
+    so=sorted(list(filter(lambda x: x>=0, A)))+sorted(list(filter(lambda x: x<0, A)))
+    print(so)
