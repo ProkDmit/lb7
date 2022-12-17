271ed5bd64757547cd419f44eb880454e1bf8a78
ProkDmit <dmidtriypdv@gmail.com>
2022-12-17 14:54:25 +0300

    1-е инд. зад.

---
+++ 7lb/1.4.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+import sys
+if __name__=='__main__':
+    A=list(map(int, input("Введите 10 элементов\n").split()))
+    if len(A)!=10:
+        print("Неверный размер массива", file=sys.stderr)
+        exit(1)
+    if min(A)<0:
+        pr=0
+        for i in range(len(A)):
+            if A[i]<0:
+                pr+=A[i]
+        print(pr)
+    else:
+        print(0)
